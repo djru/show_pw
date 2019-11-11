471920acb2705c88de2a3762e91460041b7c3cb5
Dan Ruswick <dan.ruswick@gmail.com>
2019-11-11 15:50:41 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Show PW Everywhere
-// @namespace    https://github.com/djru/show_pw
-// @version      0.1
+// @namespace    http://tampermonkey.net/
+// @version      0.2
 // @description  show password value everywhere
-// @author       djru
+// @author       You
 // @match        *://*/*
 // @grant        none
 // ==/UserScript==
@@ -15,15 +15,27 @@
     if(!pw){
         return false
     }
-    const pw_show = document.createElement('div') // div to hold our shown password
+    const pw_show = document.createElement('div')
     pw_show.innerText = 'hide'
-    pw.insertAdjacentElement('afterend', pw_show)
+    //pw.insertAdjacentElement('afterend', pw_show)
+    document.body.insertAdjacentElement('beforeend', pw_show)
     pw_show.classList.add('pw_show')
-    // set the position of the div to be directly below the pw elemebt
-    var position = pw.getBoundingClientRect()
-    
+    var position = pw.getBoundingClientRect();
+    console.log(position.left, position.bottom)
+    const styles = {
+                 position: 'absolute',
+                'z-index': 1000,
+                 background: 'rgb(234, 234, 234)',
+                 padding: '2px 10px',
+                 color: '#828282',
+                 cursor: 'pointer',
+                 left: `${position.left}px`,
+                 top: `${position.bottom}px`
+               }
+    Object.assign(pw_show.style, styles)
+    console.log(pw.style)
+
     pw.onkeyup = (e) => {
-        // don't want to fire on browser autofill, which is why we only want KeyboardEvent
         if (e instanceof KeyboardEvent){
             pw_show.innerText = e.target.value;
             if(e.target.value === '' || pw_show.dataset.show === "false"){
@@ -33,25 +45,8 @@
             }
         }
     }
-    // can click to banish
     pw_show.onclick = (e) => {
         pw_show.style.display = 'none'
         pw_show.dataset.show = false
     }
-    pw_show.style.left = `${position.left}`
-    pw_show.style.top = `${position.bottom}`
-
-    // inject some styles
-    const css = `.pw_show {
-position: absolute;
-z-index: 1000;
-background: rgb(234, 234, 234);
-padding: 2px 10px;
-color: white;
-cursor: pointer
-}`
-    const css_el = document.createElement('style');
-    css_el.type = 'text/css';
-    css_el.innerText = css
-    document.getElementsByTagName("head")[0].appendChild(css_el);
 })();
